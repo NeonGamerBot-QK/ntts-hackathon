cf61fd4b921ac1c7a692a4023934d13292215c85
mynameisbob1928 <dduuddeegguuyy@gmail.com>
2024-02-10 16:15:38 +0000

    eslint, ping command and ready.js updated

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,25 @@
+const { ActivityType } = require("discord.js");
 module.exports = {
-    name: 'ready',
-    execute: () => {
-        console.log('ready code being moved here soon')
-    }
-}
+  name: "ready",
+  once: true,
+  async execute(client) {
+    let sCount = client.guilds.cache.size;
+    const ascii =
+      "   ____        _                        _       \n  | __ )  ___ | |_   _ __ ___  __ _  __| |_   _ \n  |  _ \\ / _ \\| __| | '__/ _ \\/ _' |/ _' | | | |\n  | |_) | (_) | |_  | | |  __/ (_| | (_| | |_| |\n  |____/ \\___/ \\__| |_|  \\___|\\__,_|\\__,_|\\__, |\n                                          |___/ ";
+    console.log(
+      `Logged in as ${client.user.tag}\nThis bot is in ${sCount} servers\n${ascii}\n\n`,
+    );
+    client.user.setActivity(`${sCount} Servers`, {
+      type: ActivityType.Watching,
+    });
+    setInterval(
+      () => {
+        sCount = client.guilds.cache.size;
+        client.user.setActivity(`${sCount} Servers`, {
+          type: ActivityType.Watching,
+        });
+      },
+      5 * 1000 * 60,
+    );
+  },
+};

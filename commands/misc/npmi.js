7c619595531db01de6fff98cc7b51a63c3088ab4
Saahil <saahilattud@gmail.com>
2024-02-10 11:14:56 -0500

    npmi cmd

---
+++ commands/misc/npmi.js
@@ -0,0 +1,34 @@
+const {SlashCommandBuilder, EmbedBuilder} = require('discord.js')
+const { exec } = require('child_process')
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('npmi')
+		.setDescription('DEVS: install a package for this robot').addStringOption(o => o.setName('pkgs').setDescription("The NPM packages to install").setRequired(true)),
+	async execute(interaction) {
+		// const embed = new EmbedBuilder()
+        //     .setTitle('Pinging...')
+        //     .setColor('Blue')
+            
+        // const sent = await interaction.reply({ embeds: [embed], fetchReply: true });
+
+        // const embed2 = new EmbedBuilder()
+        //     .setTitle(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`)
+        //     .setColor('Green')
+
+        // interaction.editReply({ embeds: [embed2], ephemeral: true });
+        let pkgs = interaction.options.getString('pkgs')
+if(!pkgs) return interaction.reply('No packages provided')
+const m = await interaction.reply({ content: `Installing ${pkgs}...`, fetchReply: true })
+exec(`npm i ${pkgs}`, (err, stdout, stderr) => {
+  if(err) return message.author.send(`Error: ${err.message}`)
+  // use git add on package.json and yarn.lock using child process
+  execSync('git add package.json yarn.lock', { stdio: 'inherit' });
+  execSync(`git commit -m "➕ dep: install ${pkgs}"`, { stdio: 'inherit' });
+  execSync('git push', { stdio: 'inherit' });
+  m.edit(`\`\`\`\n${stdout}\n\`\`\``)
+  if (stderr) {
+    m.reply(`\`\`\`\n${stderr}\n\`\`\``)
+  }
+})
+	},
+};

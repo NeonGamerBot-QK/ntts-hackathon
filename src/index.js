3809fe4aacf1a29f0b0774882b87b7f9ecc409eb
Saahil <saahilattud@gmail.com>
2024-02-10 10:32:09 -0500

    e

--- src/index.js
+++ src/index.js
@@ -106,4 +106,19 @@ client.on(Events.InteractionCreate, async interaction => {
 		}
 	}
 });
+
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 client.login(process.env.DISCORD_TOKEN)

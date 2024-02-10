d49aa719a36a8ffb71799b3e77831fe2adacf46a
Saahil <saahilattud@gmail.com>
2024-02-10 10:21:59 -0500

    intents

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 const discord = require('discord.js')
 const client = new discord.Client({
-    intents: [
-        'GUILDS',
-        'GUILD_MEMBERS',
-        'GUILD_BANS',
-        'GUILD_EMOJIS_AND_STICKERS',
-        'GUILD_INTEGRATIONS',
-        'GUILD_WEBHOOKS',
-        'GUILD_INVITES',
-        'GUILD_VOICE_STATES',
-        'GUILD_PRESENCES',
-        'GUILD_MESSAGES',
-        'GUILD_MESSAGE_REACTIONS',
-        'GUILD_MESSAGE_TYPING',
-        'DIRECT_MESSAGES',
-        'DIRECT_MESSAGE_REACTIONS',
-        'DIRECT_MESSAGE_TYPING',
-        'GUILD_SCHEDULED_EVENTS'
-      ],
+    intents: Object.values(discord.GatewayIntentBits),
 })
 client.on('ready', () => {
     console.log(`Ready on ${client.user.tag}`)

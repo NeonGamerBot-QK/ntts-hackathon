0f63d0ba0c3ce099fe210557c70c93ec86853762
Neon <neon@saahild.com>
2024-02-10 23:43:02 -0500

    logs sys

---
+++ events/auditLogAbuse.js
@@ -0,0 +1,45 @@
+const { AuditLogEvent, Events } = require("discord.js");
+module.exports = {
+  name: Events.GuildAuditLogEntryCreate,
+  async execute(auditLog) {
+    const client = auditLog.client;
+    // Define your variables.
+    // The extra information here will be the channel.
+    const { action, extra: channel, executorId, targetId } = auditLog;
+
+    // Check only for deleted messages.
+    if (action == AuditLogEvent.MessageDelete) {
+      // Ensure the executor is cached.
+
+      const executor = await client.users.fetch(executorId);
+
+      // Ensure the author whose message was deleted is cached.
+      const target = await client.users.fetch(targetId);
+
+      // Log the output.
+      console.log(
+        `A message by ${target.tag} was deleted by ${executor.tag} in ${channel}.`,
+      );
+    }
+ else if (action == AuditLogEvent.MemberKick) {
+      // Ensure the executor is cached.
+      const executor = await client.users.fetch(executorId);
+
+      // Ensure the kicked guild member is cached.
+      const kickedUser = await client.users.fetch(targetId);
+
+      // Now you can log the output!
+      console.log(`${kickedUser.tag} was kicked by ${executor.tag}.`);
+    }
+ else if (action == AuditLogEvent.MemberBanAdd) {
+      // Ensure the executor is cached.
+      const executor = await client.users.fetch(executorId);
+
+      // Ensure the banned guild member is cached.
+      const bannedUser = await client.users.fetch(targetId);
+
+      // Now you can log the output!
+      console.log(`${bannedUser.tag} was banned by ${executor.tag}.`);
+    }
+  },
+};
---
+++ events/editMessage.js
@@ -0,0 +1,26 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "editMessage",
+  async execute(oldMessage, newMessage) {
+    // console.log(`A message by ${oldMessage.author.tag} was edited in ${oldMessage.channel}`);
+    const embed = new EmbedBuilder();
+    embed.setTitle("Message Edited");
+    embed.setDescription(
+      `A message by ${oldMessage.author.tag} was edited in ${oldMessage.channel}`,
+    );
+    embed.addField("Old Message", oldMessage.content);
+    embed.addField("New Message", newMessage.content);
+    embed.setTimestamp();
+    embed.setColor("RANDOM");
+    // oldMessage.channel.send({ embeds: [embed] });
+    const channelId = oldMessage.client.db.get(
+      `logchannel_${oldMessage.guild.id}_` +
+        require("../../src/static/logTypes.json")[31].value,
+    );
+    const channel = oldMessage.guild.channels.cache.get(channelId);
+    if (channel) {
+      channel.send({ embeds: [embed] });
+    }
+  },
+};

b2e39b22cd12cca05b1ebbef00e964d43dc4f669
Saahil <neon@saahild.com>
2024-02-12 22:10:49 -0500

    quickfix

--- deploy.js
+++ deploy.js
@@ -23,6 +23,7 @@ for (const folder of commandFolders) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     if ("data" in command && "execute" in command) {
+      console.log(command.data.name);
       commands.push(command.data.toJSON());
     }
  else {

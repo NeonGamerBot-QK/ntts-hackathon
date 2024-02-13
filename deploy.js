1a1917baa3c378bd3f2c9aca267d34fb87194d15
Saahil <neon@saahild.com>
2024-02-12 22:09:27 -0500

    quickfix

--- deploy.js
+++ deploy.js
@@ -42,7 +42,7 @@ const rest = new REST().setToken(token);
     console.log(
       `Started refreshing ${commands.length} application (/) commands.`,
     );
-
+    console.log(commands);
     // The put method is used to fully refresh all commands in the guild with the current set
     const data = await rest.put(
       Routes.applicationGuildCommands(clientId, guildId),

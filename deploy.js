21e0b0554bb1a2c1a1718b9e367490b9780111d4
Saahil <neon@saahild.com>
2024-02-12 22:12:11 -0500

    quickfix

--- deploy.js
+++ deploy.js
@@ -21,6 +21,7 @@ for (const folder of commandFolders) {
   // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
+    console.log(filePath);
     const command = require(filePath);
     if ("data" in command && "execute" in command) {
       console.log(command.data.name);

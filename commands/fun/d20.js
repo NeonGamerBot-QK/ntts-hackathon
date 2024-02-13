2baf5ed74b9cdf9dd5cd463e5ca431780ceabfa2
Saahil <neon@saahild.com>
2024-02-12 22:57:13 -0500

    dice cmd

---
+++ commands/fun/d20.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, AttachmentBuilder } = require("discord.js");
+const fs = require("fs");
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("d20")
+    .setDescription("Roll a 20-sided die"),
+  async execute(interaction) {
+    const roll = Math.floor(Math.random() * 20) + 1;
+    await interaction.reply({
+      content: `You rolled a ${roll}`,
+      files: [
+        new AttachmentBuilder()
+          .setName(`${roll}.mp4`)
+          .setFile(
+            fs.readFileSync(
+              require("path").resolve(`../../assets/${roll}.mp4`),
+            ),
+          ),
+      ],
+    });
+  },
+};

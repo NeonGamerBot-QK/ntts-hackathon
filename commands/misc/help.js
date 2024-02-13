cb3c60340bc3fded04eaea35e8d63f3173b04a96
Saahil <neon@saahild.com>
2024-02-12 20:24:11 -0500

    quickfix

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -29,7 +29,7 @@ module.exports = {
   async execute(interaction) {
     const command = interaction.options.getString("command");
     if (command) {
-      const cmd = interaction.client.commands.get(command);
+      const cmd = interaction.guild.commands.cache.get(command);
       if (!cmd) {
         return await interaction.reply({
           content: "That command does not exist",
@@ -37,13 +37,18 @@ module.exports = {
         });
       }
       const data = [];
-      data.push(`**Name:** ${cmd.data.name}`);
-      data.push(`**Description:** ${cmd.data.description}`);
-      if (cmd.data.options) {
+      data.push(`**Name:** ${cmd.name}`);
+      data.push(`**Description:** ${cmd.description}`);
+      if (cmd.options) {
         data.push("**Options:**");
-        cmd.data.options.forEach((option) => {
-          data.push(`**Name:** ${option.name}`);
+        cmd.options.forEach((option) => {
+          data.push(
+            `**Name:** ${option.type == 1 ? `</${cmd.name} ${option.name}:${cmd.id}>` : option.name}`,
+          );
           data.push(`**Description:** ${option.description}`);
+          // if(option.type == 1) {
+          //   // data.push(`**Type:** Sub-command`);
+          // }
           if (option.choices) {
             data.push("**Choices:**");
             option.choices.forEach((choice) => {

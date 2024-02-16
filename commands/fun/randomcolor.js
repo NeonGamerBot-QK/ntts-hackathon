fb5b09c82d79a401f7fa26625a65e017777a6f0e
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-02-17 00:15:43 +0530

    randomcolor.js updated

--- commands/fun/randomcolor.js
+++ commands/fun/randomcolor.js
@@ -5,21 +5,30 @@ module.exports = {
     .setName("randomcolor")
     .setDescription("Get a random color"),
   async execute(interaction) {
-    const color =
-      ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
-    await interaction.reply({
-      // content: color,
-      ephemeral: true,
-      embeds: [
-        {
-          title: "Random Color",
-          color: color,
-          description: color,
-          image: {
-            url: `https://www.colorhexa.com/${color.slice(1)}.png`,
-          },
-        },
-      ],
-    });
+    const randomHex = Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+
+    // Build embed message
+    const embed = new EmbedBuilder()
+      .setColor(`#${randomHex}`)
+      .setTitle('Random Color!')
+      .setDescription(`Here is your random hex color code: \n #${randomHex}`);
+
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+    // const color =
+    //   ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+    // await interaction.reply({
+    //   // content: color,
+    //   ephemeral: true,
+    //   embeds: [
+    //     {
+    //       title: "Random Color",
+    //       color: color,
+    //       description: color,
+    //       image: {
+    //         url: `https://www.colorhexa.com/${color.slice(1)}.png`,
+    //       },
+    //     },
+    //   ],
+    // });
   },
 };

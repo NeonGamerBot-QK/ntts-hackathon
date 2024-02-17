aa020f3499132d6c00f511525361592e2c2d0af9
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-02-17 10:44:26 +0530

    hi

--- commands/system/lock.js
+++ commands/system/lock.js
@@ -13,48 +13,14 @@ module.exports = {
     ),
   async execute(interaction) {
     const channel = interaction.options.getChannel("channel");
-    if (channel.type === "GUILD_CATEGORY") {
-      return interaction.reply({
-        content: "You can't lock a category",
-        ephemeral: true,
-      });
-    }
-    if (channel.type === "GUILD_VOICE") {
-      return interaction.reply({
-        content: "You can't lock a voice channel",
-        ephemeral: true,
-      });
-    }
-    if (!channel.permissionsFor(interaction.guild.me).has("MANAGE_CHANNELS")) {
-      return interaction.reply({
-        content: "I don't have permission to lock that channel",
-        ephemeral: true,
-      });
-    }
-    if (!channel.permissionsFor(interaction.member).has("MANAGE_CHANNELS")) {
-      return interaction.reply({
-        content: "You don't have permission to lock that channel",
-        ephemeral: true,
-      });
-    }
-    if (channel.permissionOverwrites.cache.has(interaction.guild.id)) {
-      const permissions = channel.permissionOverwrites.cache.get(
-        interaction.guild.id,
-      );
-      if (permissions.deny.bitfield === 0) {
-        return interaction.reply({
-          content: "The channel is already locked",
-          ephemeral: true,
-        });
-      }
-      await permissions.update({ SEND_MESSAGES: null });
-    }
- else {
-      await channel.permissionOverwrites.create(interaction.guild.id, {
+    if (channel.type === 'text') {
+      await channel.permissionOverwrites.create(interaction.guild.roles.everyone, {
         SEND_MESSAGES: false,
       });
-      await channel.send({ content: ":lock: This channel has been locked" });
+      await interaction.reply({
+        content: `Successfully locked ${channel}`,
+        ephemeral: true,
+      });
     }
-    await interaction.reply({ content: `Locked ${channel}`, ephemeral: true });
   },
 };

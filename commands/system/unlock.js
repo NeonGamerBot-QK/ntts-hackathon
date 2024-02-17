315f1bbe4bf90f80bbab0c884eb7f1585d4328a6
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-02-17 11:10:36 +0530

    hi

--- commands/system/unlock.js
+++ commands/system/unlock.js
@@ -1,9 +1,9 @@
-const { ChannelType, SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, ChannelType } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("unlock")
-    .setDescription("Unlock a channel")
+    .setDescription("Unlocks a channel")
     .addChannelOption((option) =>
       option
         .setName("channel")
@@ -13,49 +13,14 @@ module.exports = {
     ),
   async execute(interaction) {
     const channel = interaction.options.getChannel("channel");
-    if (channel.type === "GUILD_CATEGORY") {
-      return interaction.reply({
-        content: "You can't unlock a category",
+    if (channel.type == ChannelType.GuildText) {
+      channel.permissionOverwrites.edit(channel.guild.roles.everyone, {
+        SendMessages: true
+    }).catch((e) => { console.error(e) })
+      await interaction.reply({
+        content: `Successfully unlocked ${channel}`,
         ephemeral: true,
       });
     }
-    if (channel.type === "GUILD_VOICE") {
-      return interaction.reply({
-        content: "You can't unlock a voice channel",
-        ephemeral: true,
-      });
-    }
-    if (!channel.permissionsFor(interaction.guild.me).has("MANAGE_CHANNELS")) {
-      return interaction.reply({
-        content: "I don't have permission to unlock that channel",
-        ephemeral: true,
-      });
-    }
-    if (!channel.permissionsFor(interaction.member).has("MANAGE_CHANNELS")) {
-      return interaction.reply({
-        content: "You don't have permission to unlock that channel",
-        ephemeral: true,
-      });
-    }
-    if (!channel.permissionOverwrites.cache.has(interaction.guild.id)) {
-      return interaction.reply({
-        content: "The channel is already unlocked",
-        ephemeral: true,
-      });
-    }
-    const permissions = channel.permissionOverwrites.cache.get(
-      interaction.guild.id,
-    );
-    if (permissions.deny.bitfield === 0n) {
-      return interaction.reply({
-        content: "The channel is already unlocked",
-        ephemeral: true,
-      });
-    }
-    await permissions.update({ SEND_MESSAGES: null });
-    await interaction.reply({
-      content: "The channel has been unlocked",
-      ephemeral: true,
-    });
   },
 };

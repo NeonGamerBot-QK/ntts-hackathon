5c1d073f0cdf1ff5a3bb02dc069c313a6241479d
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-02-20 00:56:46 +0530

    shjkodgkjsdbjklsgd

--- commands/system/lock.js
+++ commands/system/lock.js
@@ -2,17 +2,8 @@ const {
   SlashCommandBuilder,
   EmbedBuilder,
   PermissionFlagsBits,
-  PermissionsBitField,
   ChannelType,
 } = require('discord.js')
-
-const flags = [
-  PermissionsBitField.Flags.SendMessages
-]
-
-const permissions = new PermissionsBitField(flags)
-
-console.log(permissions.bitfield);
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -28,23 +19,17 @@ module.exports = {
         .setRequired(false)
     ),
   async execute(interaction) {
+      const channel = interaction.options.getChannel('channel') || interaction.channel
       await interaction.deferReply()
-      let channel = interaction.options.getChannel('channel') || interaction.channel
-      ow = channel.permissionOverwrites.cache.get(channel.guildId);
-      console.log(ow);
-      console.log(ow.SEND_MESSAGES === true);
-      console.log(PermissionsBitField.resolve(ow.permissions));
-      if (ow && ow.SEND_MESSAGES === true) 
-      {
-        interaction.editReply({ content: "The channel is already locked." , ephemeral: true });
-      }
-      else 
-      {
-        channel.permissionOverwrites.create(interaction.guild.id, { SendMessages: false })
-        const embed = new EmbedBuilder()
-          .setColor('Red')
-          .setFooter({ text: `Done by: ${interaction.user.username}`, iconURL: `${interaction.user.avatarURL()}` })
-        await interaction.editReply({ content: `${channel} has been locked`, embeds: [embed] })
-      }
+      channel.permissionOverwrites.create(interaction.guild.id, {
+        SendMessages: false,
+      })
+      const embed = new EmbedBuilder().setTitle(`${channel} has been locked`).setColor('Red').setFooter({
+        text: `Done by: ${interaction.user.username}`,
+        iconURL: `${interaction.user.avatarURL()}`,
+      })
+      await interaction.editReply({
+        embeds: [embed],
+      })
   },
 }

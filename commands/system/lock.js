939241fb33e1335d41548dd9e5b416c27d6ba25b
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-02-19 10:24:45 +0530

    updated lock

--- commands/system/lock.js
+++ commands/system/lock.js
@@ -19,31 +19,28 @@ module.exports = {
         .setRequired(false)
     ),
   async execute(interaction) {
-    try {
-      let channel =
-        interaction.options.getChannel('channel') || interaction.channel
-
-      await interaction.deferReply()
-
-      channel.permissionOverwrites.create(interaction.guild.id, {
-        SendMessages: false,
-      })
-
-      const embed = new EmbedBuilder().setColor('Red').setFooter({
-        text: `Done by: ${interaction.user.username}`,
-        iconURL: `${interaction.user.avatarURL()}`,
-      })
-
-      await interaction.editReply({
-        content: `${channel} has been locked`,
-        embeds: [embed],
-      })
-    } catch (error) {
-      await interaction.editReply('Oops! There was an error.').then((msg) => {
-        setTimeout(() => {
-          msg.delete()
-        }, 10000)
-      })
+    try 
+    {
+      let channel = interaction.options.getChannel('channel') || interaction.channel
+      let {id} = interaction.guild.defaultRole;
+      ow = interaction.channel.permissionOverwrites.get(id); 
+      if (ow && ow.SEND_MESSAGES === false) 
+      {
+        interaction.reply({ content: "The channel is already locked." , ephemeral: true });
+      }
+      else 
+      {
+        await interaction.deferReply()
+        channel.permissionOverwrites.create(interaction.guild.id, { SendMessages: false })
+        const embed = new EmbedBuilder()
+          .setColor('Red')
+          .setFooter({ text: `Done by: ${interaction.user.username}`, iconURL: `${interaction.user.avatarURL()}` })
+        await interaction.editReply({ content: `${channel} has been locked`, embeds: [embed] })
+      }
+    } 
+    catch (error) 
+    {
+      await interaction.editReply('Oops! There was an error.').then((msg) => { setTimeout(() => {msg.delete()}, 10000)})
       console.log(error)
     }
   },

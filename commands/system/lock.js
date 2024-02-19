02cd4f5ab9dd24478ca8042903af750b917ca243
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-02-20 01:06:18 +0530

    fghfghfghf

--- commands/system/lock.js
+++ commands/system/lock.js
@@ -21,16 +21,26 @@ module.exports = {
   async execute(interaction) {
       const channel = interaction.options.getChannel('channel') || interaction.channel
       await interaction.deferReply()
+
+      if (channel.permissionOverwrites.cache.get(interaction.guild.id).deny.has(PermissionFlagsBits.SendMessages)) {
+        return interaction.editReply({
+          content: "Channel is already locked",
+        })
+      }
+
       channel.permissionOverwrites.create(interaction.guild.id, {
         SendMessages: false,
       })
+
       const embed = new EmbedBuilder().setTitle(`${channel} has been locked`).setColor('Red').setFooter({
         text: `Done by: ${interaction.user.username}`,
         iconURL: `${interaction.user.avatarURL()}`,
       })
+
       await interaction.editReply({
         embeds: [embed],
       })
+      
       await channel.send("Locked the text channel.")
   },
 }

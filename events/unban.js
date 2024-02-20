501045b78633de1a546f5eed2f009d0c4d31717e
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-02-20 15:36:06 +0530

    sd

--- events/unban.js
+++ events/unban.js
@@ -6,34 +6,38 @@ module.exports = {
     // check audit log for reason
     const embed = new EmbedBuilder();
 
-    const entry = await guild
-      .fetchAuditLogs({ type: AuditLogEvent.MemberBanRemove })
-      .then((audit) => audit.entries.first());
-    if (entry) {
-      // get the executor
-      const executor = entry.executor;
-      // get the reason
-      const reason = entry.reason;
+    try {
+      const auditLogs = await guild.fetchAuditLogs({ type: AuditLogEvent.MemberBanRemove });
+      const entry = auditLogs.entries.first();
 
-      // create a pretty embed
+      if (entry) {
+        // get the executor
+        const executor = entry.executor;
+        // get the reason
+        const reason = entry.reason;
 
-      embed.addFields(
-        { name: "UnBanned by", value: executor.tag },
-        { name: "Reason", value: reason },
+        // create a pretty embed
+        embed.addFields(
+          { name: "UnBanned by", value: executor.tag },
+          { name: "Reason", value: reason },
+        );
+      }
+
+      embed.setTitle("User Un-Banned");
+      embed.setDescription(`${user.tag} was un-banned from the server`);
+      embed.setTimestamp();
+
+      const channelId = guild.client.db.get(
+        `logchannel_${guild.id}_` +
+          require("../src/static/logTypes.json")[9].value,
       );
-    }
-    // create a pretty embed
-    embed.setTitle("User Un-Banned");
-    embed.setDescription(`${user.tag} was un-banned from the server`);
-    embed.setTimestamp();
-
-    const channelId = guild.client.db.get(
-      `logchannel_${guild.id}_` +
-        require("../src/static/logTypes.json")[9].value,
-    );
-    const channel = guild.channels.cache.get(channelId);
-    if (channel) {
-      // channel.send(`:no_entry: ${user.tag} was banned from the server`);
+      const channel = guild.channels.cache.get(channelId);
+      if (channel) {
+        // Uncomment the following line to send the embed
+        // await channel.send({ embeds: [embed] });
+      }
+    } catch (error) {
+      console.error('Error fetching audit logs or sending embed:', error);
     }
   },
 };

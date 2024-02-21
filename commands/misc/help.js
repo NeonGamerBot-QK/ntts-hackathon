c2ec2ab506a66db69862906569ba57b3ba3eb83e
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-02-22 00:16:30 +0530

    hi

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
 const fs = require('fs')
 
 module.exports = {

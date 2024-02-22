d6dc1de66e8084c08329353a272683996013a366
Saahil <saahilattud@gmail.com>
2024-02-21 22:53:32 -0500

    quickfix

--- commands/misc/buttonreactionroles.js
+++ commands/misc/buttonreactionroles.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { ButtonBuilder } = require("discord-gamecord/utils/utils");
+const {
+  SlashCommandBuilder,
+  RoleSelectMenuBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -102,6 +108,110 @@ module.exports = {
         content: "Button Reaction Role Edited",
         empheral: true,
       });
+    }
+ else if (subcommand === "list") {
+      const reactionroles = interaction.client.db
+        .all()
+        .filter((x) => x.ID.startsWith(`reactionrole_${interaction.guild.id}`))
+        .map((x) => x.ID.split("_")[2]);
+      await interaction.reply({
+        content: `Button Reaction Roles: ${reactionroles.join(", ")}`,
+        empheral: true,
+      });
+    }
+ else if (subcommand == "deploy_message") {
+      const selectMenu = new RoleSelectMenuBuilder();
+      selectMenu.setCustomId("roleselectmenu");
+      selectMenu.setPlaceholder("Select a role");
+      selectMenu.setMinValues(1);
+      selectMenu.setMaxValues(25);
+      // selectMenu.setMaxValues(1);
+      selectMenu.setPlaceholder("Select a role");
+      // selectMenu.setMaxValues
+
+      const row = ActionRowBuilder().addComponents(selectMenu);
+      await interaction
+        .reply({
+          content: "Which roles for this message?",
+          components: [row],
+          fetchReply: true,
+        })
+        .then((m) => {
+          const comp = new Array(5)
+            .join(",")
+            .split(",")
+            .map(() => new ActionRowBuilder());
+          let i = 0;
+          // create component collector
+          const filter = (ii) => ii.customId === "roleselectmenu";
+          const collector = m.createMessageComponentCollector({
+            filter,
+            time: 15000,
+          });
+          collector.on("collect", async (ii) => {
+            //   const role = i.values[0];
+            i.values.forEach((role) => {
+              if (
+                !interaction.client.db.get(
+                  `reactionrole_${interaction.guild.id}_${role.id}`,
+                )
+              ) {
+return ii.reply({
+                  content: "Role not added to the reaction roles system",
+                  ephemeral: true,
+                });
+}
+              // const member = i.member;
+              const guild = i.guild;
+              //   const channel = i.channel;
+              //   const message = i.message;
+              const roleObj = guild.roles.cache.get(role);
+              //   await member.roles.add(roleObj);
+              // interaction.client.db.set(
+              //     `reactionrole_${interaction.guild.id}_${roleObj.name}`,
+              //     roleObj.id,
+              // );
+              comp[i].addComponents(
+                new ButtonBuilder()
+                  .setLabel(
+                    interaction.client.db.get(
+                      `reactionrole_${interaction.guild.id}_${role.id}`,
+                    ).label || roleObj.name,
+                  )
+                  .setStyle(ButtonStyle.Primary)
+                  .setCustomId(roleObj.id),
+              );
+              if (comp[i].data.components.length === 5) {
+                // i.reply({ content: "Max roles reached", ephemeral: true })
+                // return;
+                i++;
+              }
+              //   comp.push()
+              // await i.deferUpdate();
+            });
+            //   if(!)
+          });
+          collector.on("end", (collected, reason) => {
+            if (reason === "time") {
+              m.edit({
+                content: "Time is up",
+                components: [],
+              });
+            }
+ else {
+              m.edit({
+                content: "Roles added",
+                //   components: comp.map((c) => c.toJSON()),
+              });
+              interaction.channel.send({
+                content: "Roles",
+                components: comp,
+              });
+            }
+          });
+          // })
+          // });
+        });
     }
   },
 };

8e7c087c727ea4d517eff1b40dbfb5df5f19eb39
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-02-23 16:13:06 +0530

    HI

--- deploy.js
+++ deploy.js
@@ -61,7 +61,7 @@ module.exports = async () => {
     // The put method is used to fully refresh all commands in the guild with the current set
     const data = await rest.put(
       Routes.applicationGuildCommands(clientId, guildId),
-      { body: [] },
+      { body: commands },
     );
 
     console.log(
